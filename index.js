452a0f379a1c23eff8efdbe9fe960e30072bfbea
Christian <cespinoza8001@gmail.com>
2021-10-03 15:58:08 -0400

    Added first game

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const headerOne = document.querySelector('h1')
 console.log('h1 log', headerOne)
 
 headerOne.style.color = 'red';
+
+colorLinks("purple");
+
+function colorLinks(hex)
+{
+    var links = document.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++)
+    {
+        if(links[i].href)
+        {
+            links[i].style.color = hex;  
+        }
+    }  
+}
